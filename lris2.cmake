a8d63781d9777d06d4be5f3cc991b0afd07d896a
Mike Langmayr <langmayr@caltech.edu>
2026-09-17 18:51:12 -0700

    Add minimal LRIS2 instrument module with config

---
+++ lris2.cmake
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------
+# @file    Instruments/lris2/lris2.cmake
+# @brief   LRIS2-specific input to the CMake build system for camerad
+# ----------------------------------------------------------------------------
+
+set (INSTRUMENT_SOURCES
+    ${CAMERAD_DIR}/Instruments/lris2/lris2_instrument.cpp
+    ${CAMERAD_DIR}/Instruments/lris2/lris2_interface_factory.cpp
+    )
