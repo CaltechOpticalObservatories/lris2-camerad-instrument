50c72933f2577c18741fcc55c3cadf472d2d36d1
Mike Langmayr <langmayr@caltech.edu>
2026-09-17 19:11:36 -0700

    Revert LRIS2 instrument module to re-land it through review

--- lris2.cmake
+++
@@ -1,9 +0,0 @@
-# ----------------------------------------------------------------------------
-# @file    Instruments/lris2/lris2.cmake
-# @brief   LRIS2-specific input to the CMake build system for camerad
-# ----------------------------------------------------------------------------
-
-set (INSTRUMENT_SOURCES
-    ${CAMERAD_DIR}/Instruments/lris2/lris2_instrument.cpp
-    ${CAMERAD_DIR}/Instruments/lris2/lris2_interface_factory.cpp
-    )
